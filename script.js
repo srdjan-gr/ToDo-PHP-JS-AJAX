d684d21bad90190248bc407fb620e77fe71430fe
srdjan <srdjan.gr@gmail.com>
2022-05-13 20:56:26 +0200

    FrontEnd / JS cleanup

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// TRanzicija kartice sa Taskovima
 let backBtn = document.getElementById('backBtn');
 let cardList = document.getElementById('cardList');
 let todoMenu = document.getElementById('todoMenu');
@@ -5,11 +6,6 @@ let todoMenu = document.getElementById('todoMenu');
 backBtn.addEventListener('click', () => {
 
     cardList.style=`
-        // left: 0;
-        // transition: left .6s ease; 
-        // transform: scale(1);
-        // transform: translateX(0rem);
-
         transform: scale(1) translateX(0rem);
         transition: transform .5s ease-out; 
     `;
@@ -18,11 +14,6 @@ backBtn.addEventListener('click', () => {
 todoMenu.addEventListener('click', () => {
 
     cardList.style=`
-        
-        // left: 22;
-        // transform: scale(.86);
-        // transition: left .5s linear;
-        
         transform: translateX(22rem) scale(.86);
         transition: transform .5s ease-out;
     `;
